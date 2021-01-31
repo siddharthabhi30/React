69cb2023a1402e96f516f5486cb068e7b70f7562
siddharth singh <you@example.com>
2021-01-31 13:49:17 +0530

    d

---
+++ tracker/react-task-tracker/src/App.js
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+
+
+function App() {
+
+  const name = 'Brad'
+  return (
+    <div className="container">
+   <Header/>
+    
+    </div>
+  );
+}
+
+export default App;
