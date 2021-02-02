59c35eb9b41cc116addb15d202e7b302d904cada
19dec2009aA1@ <siddharthabhi30@gmail.com>
2021-02-02 23:04:39 +0530

    ll

---
+++ gallery2/myproj/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="text-red-500">yo</div>
+  );
+}
+
+export default App;
--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: '#FF6363',
-        secondary:{
-          100:'#E2E2DS',
-          200:'#888883'
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
         }
+      },
+      fontFamily: {
+        body: ['Nunito']
       }
     },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
 }
